638b42d0ebc6760415cbe3aed9e6bc53879d1915
Jonjo McKay <jonjo@jonjomckay.com>
2018-01-20 23:51:44 +0000

    Added some dictionary extensions

---
+++ src/Xenolope.Extensions/DictionaryExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Xenolope.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary,
+            TKey key,
+            TValue defaultValue)
+        {
+            if (dictionary == null)
+            {
+                return defaultValue;
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+        }
+
+        public static TValue GetValueOrDefault<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary,
+            TKey key,
+            Func<TKey, TValue> defaultValueProvider)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            
+            if (dictionary == null)
+            {
+                return defaultValueProvider(key);
+            }
+
+            return dictionary.TryGetValue(key, out var value) ? value : defaultValueProvider(key);
+        }
+    }
+}
---
+++ test/Xenolope.Extensions.Tests/DictionaryExtensionsTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Xenolope.Extensions.Tests
+{
+    public class DictionaryExtensionsTests
+    {
+        [Fact]
+        public void TestGetValueOrDefaultWithNullDictionary()
+        {
+            Dictionary<string, string> dictionary = null;
+
+            Assert.Equal("default value", dictionary.GetValueOrDefault("key", "default value"));
+        }
+
+        [Fact]
+        public void TestGetValueOrDefaultWithEmptyDictionary()
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            Assert.Equal("default value", dictionary.GetValueOrDefault("key", "default value"));
+        }
+
+        [Fact]
+        public void TestGetValueOrDefaultWithValidDictionary()
+        {
+            var dictionary = new Dictionary<string, string>
+            {
+                { "key", "value" }
+            };
+
+            Assert.Equal("value", dictionary.GetValueOrDefault("key", "default value"));
+        }
+
+        [Fact]
+        public void TestGetValueOrDefaultWithInvalidKey()
+        {
+            var dictionary = new Dictionary<string, string>
+            {
+                { "key", "value" }
+            };
+
+            Assert.Throws<ArgumentNullException>("key", () => dictionary.GetValueOrDefault(null, "default value"));
+        }
+
+        [Fact]
+        public void TestGetValueOrDefaultWithProviderWithNullDictionary()
+        {
+            Dictionary<string, string> dictionary = null;
+
+            Assert.Equal("default value from key",
+                dictionary.GetValueOrDefault("key", key => "default value from " + key));
+        }
+
+        [Fact]
+        public void TestGetValueOrDefaultWithProviderWithEmptyDictionary()
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            Assert.Equal("default value from key",
+                dictionary.GetValueOrDefault("key", key => "default value from " + key));
+        }
+
+        [Fact]
+        public void TestGetValueOrDefaultWithProviderWithValidDictionary()
+        {
+            var dictionary = new Dictionary<string, string>
+            {
+                { "key", "value" }
+            };
+
+            Assert.Equal("value", dictionary.GetValueOrDefault("key", key => "default value from " + key));
+        }
+
+        [Fact]
+        public void TestGetValueOrDefaultWithProviderWithInvalidKey()
+        {
+            var dictionary = new Dictionary<string, string>
+            {
+                { "key", "value" }
+            };
+
+            Assert.Throws<ArgumentNullException>("key", () => dictionary.GetValueOrDefault(null, key => null));
+        }
+    }
+}
